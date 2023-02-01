5792f3f757b0f29a7110e1cc3a6454c56e1bc197
MultimedieGunvor <eaagupa@students.eaaa.dk>
2023-02-01 11:56:26 +0100

    Testing adding to the firestore database

---
+++ src/pages/Station.js
@@ -0,0 +1,16 @@
+import { collection, addDoc } from "firebase/firestore";
+
+
+export default async function Station() {
+    
+    try {
+        const docRef = await addDoc(collection(db, "wagons"), {
+            first: "Ada",
+            last: "Lovelace",
+            born: 1815
+        });
+        console.log("Documentwritten with ID: ", docRef.id);
+    } catch (e) {
+        console.error("Error adding document", e);
+    }
+}
