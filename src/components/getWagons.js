f755412ca8eadcd2bb484624e36ff7384b0c7d53
MultimedieGunvor <eaagupa@students.eaaa.dk>
2023-02-02 15:20:36 +0100

    bhjkvh

--- src/components/getWagons.js
+++ src/components/getWagons.js
@@ -5,17 +5,15 @@ import { useState } from "react";
 export default function GetWagons() {
 
     const [Wagons, SetWagons] = useState([]);
-    const [state, setState] = useState(Wagons); //<--- Prøv dette efter frokost
 
     async function getAllWagons() {
 
         const querySnapshot = await getDocs(collection(db, "wagons"));
+
         querySnapshot.forEach((doc) => {
             const Wagons = [doc.id, doc.data()]; 
             SetWagons(Wagons);
-            // console.log(doc.id, " => ", doc.data());
             console.log(Wagons);
-            setState(Wagons); // <--------- Prøv dette efter frokost
         });
     }
 
@@ -26,15 +24,16 @@ export default function GetWagons() {
             {Wagons.length === 0 ? (
                 <p>No wagons in database</p>
             ) : (
-                state.map((item) => (
-                    <div className="single-wagon" key={item.id}>
+                Wagons.map((item) => (
+                    <div className="single-wagon" key={Wagons.indexOf(0)}>
+                        <p>{item.key}</p>
                         <p>{item.first}</p>
                         <p>{item.middle}</p>
                         <p>{item.last}</p>
                         <p>{item.born}</p>
                     </div>
                 ))
-            )} {/* <------ Prøv dette efter frokost */}
+            )} 
         </>
     )
 }
