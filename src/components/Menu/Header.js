b09c59edf941a65a7f89dd869689293f7edd512e
MultimedieGunvor <eaagupa@students.eaaa.dk>
2023-02-07 12:51:37 +0100

    Added icons and styling

--- src/components/Menu/Header.js
+++ src/components/Menu/Header.js
@@ -2,13 +2,17 @@
 
 import Navbar from './Navbar';
 import { Link } from 'react-router-dom';
+import logo from "../../graphics/Logo.svg";
 
 export default function Header() {
   return (
     <header>
       <div className="nav-area">
         <Link to="/" className='logo'>
-            Logo
+            <img src={logo} alt="Track Overview logo" width={"50px"} />
+            <div>
+              <h3>Track <br />Overview </h3>
+            </div>
         </Link>
         <Navbar />
       </div>
