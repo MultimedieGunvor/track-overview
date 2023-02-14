a996ddefdfb80779803f356b87cfc99ce5f4f85e
Giulia Ivan <eaagril@students.eaaa.dk>
2023-02-14 10:47:27 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
 // styles
-import './index.css';
 // import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
