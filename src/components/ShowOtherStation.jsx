a3719e3a2b5c99bb4d55528fb8ea2daba3df8ebf
MultimedieGunvor <eaagupa@students.eaaa.dk>
2023-02-16 08:59:38 +0100

    Testing drag and drop in OtherStation

--- src/components/ShowOtherStation.jsx
+++ src/components/ShowOtherStation.jsx
@@ -1,7 +1,49 @@
+import React, { useState, useRef } from "react";
 
 export default function OtherStation () {
+    const dragItem = useRef();
+    const dragOverItem = useRef();
+    const [list, setList] = useState(['Item 1','Item 2','Item 3','Item 4','Item 5','Item 6']); /* --- Items to be dragged --- */
+ 
+    /* --- Locate the items to be dragged --- */
+    const dragStart = (e, position) => {
+        dragItem.current = position;
+        console.log(e.target.innerHTML);
+    };
+ 
+    /* --- Which element is the dragged element floating on? --- */ 
+    const dragEnter = (e, position) => {
+        dragOverItem.current = position;
+        console.log(e.target.innerHTML);
+  };
+ 
+  /* --- Insert dragged item and rearrange the list of items. Here we would probably need to add a function to reassign the item's track/position in the database --- */  
+  const drop = (e) => {
+        const copyListItems = [...list];
+        const dragItemContent = copyListItems[dragItem.current];
+        copyListItems.splice(dragItem.current, 1);
+        copyListItems.splice(dragOverItem.current, 0, dragItemContent);
+        dragItem.current = null;
+        dragOverItem.current = null;
+        setList(copyListItems);
+    };
 
     return (
-        <p>This station is awaiting construction.<br/>Gunvor will try out her drag-n-drop idea here.</p>
-    )
-}
+        
+        <>
+            <p>This station is awaiting construction.<br/>Gunvor will try out her drag-n-drop idea here.</p>
+            {
+            list&&
+            list.map((item, index) => (
+            <div style={{backgroundColor:'lightblue', margin:'20px 25%', textAlign:'center', fontSize:'40px'}}
+                onDragStart={(e) => dragStart(e, index)}
+                onDragEnter={(e) => dragEnter(e, index)}
+                onDragEnd={drop}
+                key={index}
+                draggable>
+                {item}
+            </div>
+            ))}
+        </>
+    );
+};
