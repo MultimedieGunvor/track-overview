441c85831e01475b21422fb905b1784a9b86ea2c
MultimedieGunvor <eaagupa@students.eaaa.dk>
2023-03-09 15:31:11 +0100

    More styling

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,13 +4,13 @@ import DeleteWagon from "./DeleteWagon";
 const Modal = ({props}) => {
     return (
         <div className="wagon-info" style={{fontSize: '12px'}}>
-            <p>{props.track}</p>
-            <p>{props.position}</p>
-            <p>{props.destination}</p>
-            <p>{props.wagonId}</p>
-            <p>{props.comment}</p>
-            <p>{props.litra}</p>
-            <p>{props.damage}</p>
+            <p>Track: {props.track.toUpperCase()}</p>
+            <p>Position: {props.position}</p>
+            <p>Destination: {props.destination}</p>
+            <p>ID: {props.wagonId}</p>
+            <p>Comments: {props.comment}</p>
+            <p>Litra: {props.litra}</p>
+            <p>Damage: {props.damage}</p>
             <DeleteWagon id={props.id}/>
         </div>
     );
